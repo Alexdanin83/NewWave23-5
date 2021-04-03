35c21f3700504a321aeed7b990a6a50dea8a7ce2
Your Name <you@example.com>
2021-04-03 09:05:14 +0200

    Add endpoints and 1 test

--- models/concerts.model.js
+++ models/concerts.model.js
@@ -5,7 +5,7 @@ const concertsSchema = new mongoose.Schema({
   genre: { type: String, required: true },
   price: { type: Number, required: true },
   day: { type: Number, required: true },
-  image: { type: String, required: true }
+  image: { type: String, required: false }
   });
 
 module.exports = mongoose.model('Concert', concertsSchema);
--- tests/api/get.test.js
+++ tests/api/get.test.js
@@ -1,51 +1,39 @@
-
 const chai = require('chai');
+const mongoose = require('mongoose');
 const chaiHttp = require('chai-http');
-const server = require('../../server.js');
+const serverModule = require('../../server');
 const Concert = require('../../models/concerts.model');
+const Seat = require('../../models/seats.model');
+
+const { server } = serverModule;
+const { dbURI } = serverModule;
+
 chai.use(chaiHttp);
 
-const expect = chai.expect;
-const request = chai.request;
+const { expect } = chai;
+const { request } = chai;
+
+describe('GET /api/concerts', async () => {
+  before(async () => { //eslint-disable-line
+    mongoose.connect('mongodb://localhost:27017/NewWaveDB',  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = mongoose.connection;
+    db.once('open', () => {
+      
+      console.log('Connected to the database');
+    });
+    db.on('error', err => {
+      console.log(`Error ${err}`);
+    });
+  });
 
 
-describe('GET /api/concerts', () => {
   it('/ should return all concerts', async () => {
-
-  const res = await request(server).get('/api/concerts');
-  expect(res.status).to.be.equal(200);
-  expect(res.body).to.be.an('array');
-  expect(res.body.length).to.be.equal(3);
-
-});
-/*  before(async () => {
-    const testDepOne = new Concert({ id: '5d9f1140f10a81216cfd4408', performer: 'performer', genre: 'genre 1', price: 25, day: 1,image: 'image1' });
-    await testDepOne.save();
-
-    const testDepTwo = new Concert({ id: '5d9f1140f10a81216cfw4409', performer: 'performer 2', genre: 'genre 2', price: 40, day: 2, image: 'image2' });
-    await testDepTwo.save();
-    console.log(await Concert.find());
-  });*/
-
-  /*it('should return concerts', async () => {
-    const res = await request(server).get('/concerts/performer/John Doe');
-      expect(res.status).to.be.equal(200);
-      expect(res.body).to.be.an('array');
-      expect(res.body.length).to.be.equal(1);
-      console.log(res.body);
-
-  });*/
-
-/*  it('/:id should return one department by :id ', async () => {
-    const res = await request(server).get('/api/departments/5d9f1140f10a81216cfd4408');
-     expect(res.status).to.be.equal(200);
-     expect(res.body).to.be.an('object');
-     expect(res.body).to.not.be.null;
-  });*/
-
-
-/*  after(async () => {
-  await Concert.deleteMany();
-});*/
+    const res = await request(server).get('/api/concerts');
+    expect(res.status).to.be.equal(200);
+  
+  });
 
 });
